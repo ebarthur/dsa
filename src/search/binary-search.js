2eb5f320980180583f993bb0e4dc5f622da06078
lasagna <arthurebenezer@aol.com>
2024-04-28 09:52:57 +0000

    search

---
+++ src/search/binary-search.js
@@ -0,0 +1,44 @@
+//  Given a SORTED array of 'n' elements and a target element 't', find the index of 't'
+// in the array. Return -1 if the target element is not found
+
+// Iterative Binary Search
+function binarySearch(array, target) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (target === array[middle]) {
+      return middle;
+    }
+    if (target < array[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return -1;
+}
+
+// Recursive Binary Search
+function recursiveBinarySearch(array, target) {
+  return search(array, target, 0, arr.length - 1);
+}
+const search = (array, target, left, right) => {
+  if (left > right) {
+    return -1;
+  }
+  let middle = Math.floor((left + left) / 2);
+  if (target === array[middle]) {
+    return middle;
+  }
+  if (target < array[middle]) {
+    return search(array, target, left, middle - 1);
+  } else {
+    return search(array, target, middle + 1, right);
+  }
+};
+
+// Big-O - O(log n)
+// Space Complexity - O(1)
---
+++ src/search/linear-search.js
@@ -0,0 +1,12 @@
+// Linear search is a simple search algorithm that finds the position of a target value
+// within an array.
+
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+//Big-O - O(n)
