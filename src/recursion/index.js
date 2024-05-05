17c3169ec361cdeb0a5c6dd395c3873575ccdcd4
lasagna <arthurebenezer@aol.com>
2024-05-05 22:43:08 +0000

    added arraysum

--- src/recursion/index.js
+++ src/recursion/index.js
@@ -10,13 +10,13 @@ function countTo(max, current, list) {
 
 const counts = countTo(5, 1, []);
 
-// The base case is when we stop recursing. If we don't have a base case,
-// then our recursion will spiral out of control and we will get a stack
+// The base case is when we stop recurring. If we don't have a base case,
+// then our recursion will spiral out of control, and we will get a stack
 // overflow (hence the name of the helpful website) when we run out of memory.
 
 function fibonnaci(n) {
   // base case
-  if (n === 1 || n == 2) {
+  if (n === 1 || n === 2) {
     return 1;
   } else if (n < 1) {
     return 0;
@@ -47,3 +47,21 @@ function factorial(n) {
 // 5! = 5 * (4 * (3 * (2 * 1))) = 120
 // So basically each iteration multiplies the number by the previous number until it gets to 1
 // Big-O = O(n)
+
+
+// ArraySum is a function that takes an array of numbers and returns the sum of all the numbers in the array
+// The function uses recursion to calculate the sum of the numbers in the array
+
+function arraySum(array) {
+  // base case
+  if (array.length === 0) {
+    return 0;
+  }
+
+  // recursive call
+  return array[0] + arraySum(array.slice(1));
+}
+
+// Big-O = O(n) because it will iterate through all the elements in the array
+
+console.log(arraySum([1, 2, 3, 4, 5])); // 15
